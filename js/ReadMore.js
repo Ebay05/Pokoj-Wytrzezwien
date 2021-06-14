2f916dab27ed43bbf18514b0ce3be810144020ea
Ebay05 <ebay11990011@gmail.com>
2021-06-14 21:10:42 +0200

    Whole SPA

---
+++ js/ReadMore.js
@@ -0,0 +1,77 @@
+// READ MORE TEXT 1
+const readMoreBtn = document.querySelector(".read-more-btn");
+const text = document.querySelector(".tekst");
+
+readMoreBtn.addEventListener("click",()=>{
+    text.classList.toggle("show-more");
+    if(readMoreBtn.innerText === 'CZYTAJ WIĘCEJ'){
+    readMoreBtn.innerText ='czytaj mniej';
+    }else{
+    readMoreBtn.innerText = 'CZYTAJ WIĘCEJ';
+    }
+});
+
+// READ MORE TEXT 2
+const readMoreBtn1 = document.querySelector(".read-more-btn1");
+const text1 = document.querySelector(".tekst1");
+
+readMoreBtn1.addEventListener("click",()=>{
+    text1.classList.toggle("show-more");
+    if(readMoreBtn1.innerText === 'CZYTAJ WIĘCEJ'){
+    readMoreBtn1.innerText ='czytaj mniej';
+    }else{
+    readMoreBtn1.innerText = 'CZYTAJ WIĘCEJ';
+    }
+});
+
+// READ MORE TEXT 3
+const readMoreBtn2 = document.querySelector(".read-more-btn2");
+const text2 = document.querySelector(".tekst2");
+
+readMoreBtn2.addEventListener("click",()=>{
+    text2.classList.toggle("show-more");
+    if(readMoreBtn2.innerText === 'CZYTAJ WIĘCEJ'){
+    readMoreBtn2.innerText ='czytaj mniej';
+    }else{
+    readMoreBtn2.innerText = 'CZYTAJ WIĘCEJ';
+    }
+});
+
+// READ MORE TEXT 4
+const readMoreBtn3 = document.querySelector(".read-more-btn3");
+const text3 = document.querySelector(".tekst3");
+
+readMoreBtn3.addEventListener("click",()=>{
+    text3.classList.toggle("show-more");
+    if(readMoreBtn3.innerText === 'CZYTAJ WIĘCEJ'){
+    readMoreBtn3.innerText ='czytaj mniej';
+    }else{
+    readMoreBtn3.innerText = 'CZYTAJ WIĘCEJ';
+    }
+});
+
+// READ MORE TEXT 5
+const readMoreBtn4 = document.querySelector(".read-more-btn4");
+const text4 = document.querySelector(".tekst4");
+
+readMoreBtn4.addEventListener("click",()=>{
+    text4.classList.toggle("show-more");
+    if(readMoreBtn4.innerText === 'CZYTAJ WIĘCEJ'){
+    readMoreBtn4.innerText ='czytaj mniej';
+    }else{
+    readMoreBtn4.innerText = 'CZYTAJ WIĘCEJ';
+    }
+});
+
+// READ MORE TEXT 6
+const readMoreBtn5 = document.querySelector(".read-more-btn5");
+const text5 = document.querySelector(".tekst5");
+
+readMoreBtn5.addEventListener("click",()=>{
+    text5.classList.toggle("show-more");
+    if(readMoreBtn5.innerText === 'CZYTAJ WIĘCEJ'){
+    readMoreBtn5.innerText ='czytaj mniej';
+    }else{
+    readMoreBtn5.innerText = 'CZYTAJ WIĘCEJ';
+    }
+});
---
+++ js/main.js
@@ -0,0 +1,95 @@
+// PRELOADER
+
+window.addEventListener('load', () => {
+    const preloader = document.querySelector('.preloader');
+    preloader.classList.add('close-preloader');
+});
+
+// SWITCHER
+
+    // checkbox input value
+    var themeSwitcher = document.querySelector('.theme-switcher input');
+    // get current theme from localStorage
+    var currentTheme = localStorage.getItem('theme');
+    
+    // check what is current theme right now and activate it
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+        if (currentTheme === 'dark') {
+            themeSwitcher.checked = true;
+        }
+    }
+    
+    // swithc between themes
+    function switchTheme(e) {
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+        else {        
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        }    
+    }
+    
+    // event listener on checkbox change
+    themeSwitcher.addEventListener('change', switchTheme, false);
+
+// UNFOLDING FOOTER
+
+const socialLinks = document.querySelector('.footer-social').addEventListener("click", function(){
+    const socialMediaDiv = document.querySelector(".social-media");
+    socialMediaDiv.classList.toggle("social-media-visible");
+});
+
+// TIMER
+
+function countDown() {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    const stream = "Jun 11, 2021 18:00:00",
+        countDown = new Date(stream).getTime(),
+
+        // setInterval ustawia czas wykonywania sie funkcji
+        interval = setInterval(function() {    
+  
+          let now = new Date().getTime(),
+              distance = countDown - now;
+  
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          // Zachowanie po skonczeniu odliczania
+          if (distance < 0) {
+
+            document.querySelector("#ended-counting").innerText = "Stream trwa! Wejdź na twitcha z linku poniżej";
+            document.getElementById("days").innerText = 0,
+            document.getElementById("hours").innerText = 0,
+            document.getElementById("minutes").innerText = 0,
+            document.getElementById("seconds").innerText = 0;
+
+            // Dodawanie dni po zakonczeniu
+            let newStreamDate = new Date(stream); console.log(newStreamDate)
+            
+            if(newStreamDate.getDay()==5){
+                // +3 dni
+                newStreamDate.setDate(newStreamDate.getDate()+3);
+            }else{
+                // +2 dni
+                newStreamDate.setDate(newStreamDate.getDate()+2);
+            }
+            
+            newStreamDate.setHours(newStreamDate.getHours()+4);
+            clearInterval(interval);
+          }
+
+        }, 0)
+    };
+
+    countDown();
+
