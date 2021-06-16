bfc175b2d0245fe5a795b516a5b58b4372c3c320
Ebay05 <ebay11990011@gmail.com>
2021-06-16 21:54:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,12 +7,9 @@ window.addEventListener('load', () => {
 
 // SWITCHER
 
-    // checkbox input value
     var themeSwitcher = document.querySelector('.theme-switcher input');
-    // get current theme from localStorage
     var currentTheme = localStorage.getItem('theme');
     
-    // check what is current theme right now and activate it
     if (currentTheme) {
         document.documentElement.setAttribute('data-theme', currentTheme);
         if (currentTheme === 'dark') {
@@ -20,7 +17,6 @@ window.addEventListener('load', () => {
         }
     }
     
-    // swithc between themes
     function switchTheme(e) {
         if (e.target.checked) {
             document.documentElement.setAttribute('data-theme', 'dark');
@@ -32,7 +28,6 @@ window.addEventListener('load', () => {
         }    
     }
     
-    // event listener on checkbox change
     themeSwitcher.addEventListener('change', switchTheme, false);
 
 // UNFOLDING FOOTER
@@ -50,7 +45,7 @@ function countDown() {
           hour = minute * 60,
           day = hour * 24;
   
-    const stream = "Jun 16, 2021 18:00:00",
+    const stream = "Jun 18, 2021 18:00:00",
         countDown = new Date(stream).getTime(),
 
         // setInterval ustawia czas wykonywania sie funkcji
@@ -62,35 +57,42 @@ function countDown() {
             document.getElementById("days").innerText = Math.floor(distance / (day)),
             document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
             document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / (second));
   
           // Zachowanie po skonczeniu odliczania
           if (distance < 0) {
-
             document.querySelector("#ended-counting").innerText = "Stream trwa! Wejdź na twitcha z linku poniżej";
-            document.getElementById("days").innerText = 0,
-            document.getElementById("hours").innerText = 0,
-            document.getElementById("minutes").innerText = 0,
+            document.getElementById("days").innerText = 0;
+            document.getElementById("hours").innerText = 0;
+            document.getElementById("minutes").innerText = 0;
             document.getElementById("seconds").innerText = 0;
+        }
 
-            // Dodawanie dni po zakonczeniu
-            let newStreamDate = new Date(stream); 
-                        
-            if(newStreamDate.getDay()==5){
-                // +3 dni
-                newStreamDate.setDate(newStreamDate.getDate()+3);
-            }else{
-                // +2 dni
-                newStreamDate.setDate(newStreamDate.getDate()+2);
-            }
-            
-            newStreamDate.setHours(newStreamDate.getHours()+4);
-
-            clearInterval(interval);
-          }
-
-        }, 0)
+        }, 1000)
     };
 
     countDown();
 
+
+    // Nieudana proba stworzenia algorytmu dodajacego 2 lub 3 dni 4h po zakończeniu odliczania (mechanizm dobry, ale kafelski sie nie odswiezaja)
+            // // Dodawanie dni po zakonczeniu
+            // let newStreamDate = new Date(stream); 
+            // document.querySelector("#ended-counting").innerText += "\n Stara data: " + newStreamDate;
+
+            // if(newStreamDate.getHours()>18 || newStreamDate.getHours()<22){
+            //     document.querySelector("#ended-counting").innerText = "Stream trwa! Wejdź na twitcha z linku poniżej";
+            //     document.getElementById("days").innerText = 0,
+            //     document.getElementById("hours").innerText = 0,
+            //     document.getElementById("minutes").innerText = 0,
+            //     document.getElementById("seconds").innerText = 0;
+            // }
+
+            // if(newStreamDate.getDay()==5){
+            //     // +3 dni
+            //     newStreamDate.setDate(newStreamDate.getDate()+3);
+            // }else{
+            //     // +2 dni
+            //     newStreamDate.setDate(newStreamDate.getDate()+2);
+            // }
+
+            // newStreamDate.setHours(newStreamDate.getHours()+4);
